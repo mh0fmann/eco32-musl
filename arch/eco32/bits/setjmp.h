4a68b7e49b6c1ba1a16b79cd65433e80dfc99a2a
Martin Hofmann <martin.hofmann@mni.thm.de>
2018-10-14 18:27:47 +0200

    setjmp: remove tls pointer from jumpbuffer. fixed sigsetjmp

--- arch/eco32/bits/setjmp.h
+++ arch/eco32/bits/setjmp.h
@@ -1,12 +1,6 @@
-/*
- * The eco32 ABI tells us which registers we need to save
- * and so the jump buffer looks like this
- * We also save $27 because it holds the thread pointer
- */
 typedef struct {
-	unsigned long callee[8];	/* local vars, callee save */
-	unsigned long tp;			/* thread pointer */
-	unsigned long fp;			/* frame pointer */
-	unsigned long sp;			/* stack pointer */
-	unsigned long fr;			/* function return */
+    unsigned long callee[8];    /* local vars, callee save */
+    unsigned long fp;           /* frame pointer */
+    unsigned long sp;           /* stack pointer */
+    unsigned long fr;           /* function return */
 } __jmp_buf;
